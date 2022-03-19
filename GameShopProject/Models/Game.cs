b53411991881c37f91e0e92707ec6be0c5aa7fbc
GiaSoPas <vanyaulitin@gmail.com>
2022-03-20 00:27:27 +0300

    Add Admin Panel

--- GameShopProject/Models/Game.cs
+++ GameShopProject/Models/Game.cs
@@ -1,7 +1,10 @@
-﻿namespace GameShop.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameShop.Models;
 
 public class Game
-{
+{   
+    [Key]
     public int Id { get; set; }
 
     public string Name { get; set; }
---
+++ GameShopProject/ViewModels/CreateUpdateGameModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameShopProject.ViewModels;
+
+public class CreateUpdateGameModel
+{
+    public int Id {get; set;}
+    
+    [Required(ErrorMessage = "Не указано имя")]
+    public string Name { get; set; }
+    
+    [Required(ErrorMessage = "Не указано описание")]
+    public string Description { get; set; }
+    
+    [Required(ErrorMessage = "Не указана цена")]
+    public decimal Price { get; set; }
+}
